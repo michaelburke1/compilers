7671107ed1cde270ef27a67d8a4f1f2c22add312
michaelburke1 <michaeljburke18@gmail.com>
2017-12-05 13:30:21 -0500

    files

--- parser2/param_list.c
+++ parser2/param_list.c
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 
 
-struct param_list * param_list_create( char *name, struct type *type, struct param_list *next ) {
+struct param_list * param_list_create( char *name, struct type *type, struct param_list *next) {
 
     struct param_list *p = malloc(sizeof(*p));
     p->name = name;
